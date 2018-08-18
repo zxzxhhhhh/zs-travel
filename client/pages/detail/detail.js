c6673fbf3769ce0449c0bbf428a1945bc1973b6f
xu <zxmaige@163.com>
2018-08-18 22:05:16 +0800

    预定界面 功能实现 需判断是否登陆

--- client/pages/detail/detail.js
+++ client/pages/detail/detail.js
@@ -1,6 +1,8 @@
 // pages/details/detail.js
 const config = require('../../config.js')
 const qcloud = require('../../vendor/wafer2-client-sdk/index.js');
+// 在需要使用的js文件中，导入js
+var util = require('../../utils/util.js');
 Page({
 
   /**
@@ -9,6 +11,7 @@ Page({
   data: {
     product: {},
     date: '',
+    count: 1,
     trueName: "",
     phone: "",
     isName: false,
@@ -16,6 +19,27 @@ Page({
   },
   changeDate(e) {
     this.setData({ date: e.detail.value });
+  },
+  onTapAdjust(event) {
+    let dataset = event.currentTarget.dataset
+    let adjustType = dataset.type
+
+    let count = this.data.count
+
+      if (adjustType == 'add') {
+        count++
+      }
+      else if (adjustType == 'minus') {
+        if (count <= 1) {
+          count = 1
+        } else {
+          count--
+        }
+      }
+    this.setData({
+      count
+    })
+
   },
   userNameInput(e) {
     //用户姓名
@@ -92,20 +116,34 @@ Page({
 
   orderNow(){
     console.log('order now!')
-    // product 中包含this.data.product 所有属性以及count属性
-    let product = Object.assign({
-      count: 1
-    }, this.data.product)
+  
+    let date = this.data.date
+    let count = this.data.count
+    let name = this.data.trueName
+    let tel = this.data.phone
+    let product_id = this.data.product.id
+
+    if(!this.data.isName || !this.data.isPhone){
+      wx.showToast({
+        icon: 'none',
+        title: '请添加相关信息！',
+      })
+      return
+    }
+
     wx.showLoading({
-      title: '预定中。。。',
+      title: '预定中..',
     })
     qcloud.request({
       url: config.service.addOrder,
       login: true,
       method: 'POST',
       data: {
-        list: [product],
-        isInstantBuy: true
+        product_id,
+        date,
+        count,
+        name,
+        tel
       },
       success: result => {
         console.log(result.data)
@@ -129,7 +167,7 @@ Page({
         console.log(result)
         wx.showToast({
           icon: 'none',
-          title: '商品购买失败',
+          title: '预定失败',
         })
       }
     })
@@ -140,5 +178,12 @@ Page({
   onLoad: function (options) {
     let productID = options.id
     this.getProduct(productID)
+    // 调用函数时，传入new Date()参数，返回值是日期和时间
+    var date = util.formatTime(new Date());
+    // 再通过setData更改Page()里面的data，动态更新页面的数据
+    this.setData({
+      date: date
+    });
+
   },
 })
